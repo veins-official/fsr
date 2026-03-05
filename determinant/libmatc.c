a63ae3f7f6d046f1f07090bd8f46c188fa931ffa
Ilya <ilya.xolodov07@mail.ru>
2026-03-05 19:55:30 +0300

    + matrix create

--- determinant/libmatc.c
+++ determinant/libmatc.c
@@ -18,4 +18,36 @@ static inline int double_eq(double a, double b) {
   return fabs(a - b) <= fmax(fabs(a), fabs(b)) * DOUBLE_EPSILON;
 }
 
+MatrixError matrix_create(Matrix** matrix, int rows, int cols) {
+  if (!matrix) return MATRIX_NULL_POINTER;
+  if (rows <= 0 || cols <= 0) return MATRIX_INVALID_SIZE;
+
+  *matrix = (Matrix*)malloc(sizeof(Matrix));
+  if (!(*matrix)) return MATRIX_MEMORY_ERROR;
+
+  (*matrix)->rows = rows;
+  (*matrix)->cols = cols;
+  (*matrix)->data = (double**)malloc(rows * sizeof(double*));
+
+  if (!(*matrix)->data) {
+    free(*matrix);
+    *matrix = NULL;
+    return MATRIX_MEMORY_ERROR;
+  }
+
+  for (int i = 0; i < rows; i++) {
+    (*matrix)->data[i] = (double*)malloc(cols, sizeof(double));
+    if (!(*matrix)->data[i]) {
+      for (int j = 0; j < i; j++) {
+        free((*matrix)->data[j]);
+      }
+      free((*matrix)->data);
+      free(*matrix);
+      *matrix = NULL;
+      return MATRIX_MEMORY_ERROR;
+    }
+  }
+
+  return MATRIX_SUCCESS;
+}
 
