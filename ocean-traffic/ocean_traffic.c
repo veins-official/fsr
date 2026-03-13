dfb5730aecdece7e27de8b59174e591bf690bc04
Ilya <veins-official@mail.ru>
2026-03-14 01:13:32 +0300

    Add write_png wrapper

--- ocean-traffic/ocean_traffic.c
+++ ocean-traffic/ocean_traffic.c
@@ -1,47 +1,46 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 #include "lodepng.h"
 #include "ocean_traffic.h"
 
 ot_error_t ot_error_from_lodepng(unsigned lodepng_error) {
-    switch (lodepng_error) {
-        case 0:  return OT_SUCCESS;
-        
-        case 78: return OT_ERR_FILE_NOT_FOUND;
-        case 79: return OT_ERR_FILE_OPEN_WRITE;
-        case 48: return OT_ERR_FILE_EMPTY;
-        
-        case 83: return OT_ERR_MEMORY_ALLOC;
-        case 77:
-        case 92:
-        case 105: return OT_ERR_MEMORY_OVERFLOW;
-        
-        case 28: return OT_ERR_PNG_SIGNATURE;
-        case 57: return OT_ERR_PNG_CRC;
-        case 58: return OT_ERR_PNG_ADLER;
-        case 30:
-        case 35: return OT_ERR_PNG_TRUNCATED;
-        case 31: return OT_ERR_PNG_COLOR_TYPE;
-        case 32:
-        case 33:
-        case 34: return OT_ERR_PNG_CHUNK;
-        case 37: return OT_ERR_PNG_BIT_DEPTH;
-        case 38:
-        case 39:
-        case 103: return OT_ERR_PNG_PALETTE;
-        case 93: return OT_ERR_PNG_DIMENSIONS;
-        case 106: return OT_ERR_PNG_PALETTE;
-        
-        case 60:
-        case 61:
-        case 90: return OT_ERR_PNG_WINDOW_SIZE;
-        case 84: return OT_ERR_PNG_ENCODE;
-        case 68:
-        case 71: return OT_ERR_PNG_ENCODE;
-        
-        default: return OT_ERR_UNKNOWN;
-    }
+  switch (lodepng_error) {
+    case 0:  return OT_SUCCESS;
+
+    case 78: return OT_ERR_FILE_NOT_FOUND;
+    case 79: return OT_ERR_FILE_OPEN_WRITE;
+    case 48: return OT_ERR_FILE_EMPTY;
+
+    case 83: return OT_ERR_MEMORY_ALLOC;
+    case 77:
+    case 92:
+    case 105: return OT_ERR_MEMORY_OVERFLOW;
+
+    case 28: return OT_ERR_PNG_SIGNATURE;
+    case 57: return OT_ERR_PNG_CRC;
+    case 58: return OT_ERR_PNG_ADLER;
+    case 30:
+    case 35: return OT_ERR_PNG_TRUNCATED;
+    case 31: return OT_ERR_PNG_COLOR_TYPE;
+    case 32:
+    case 33:
+    case 34: return OT_ERR_PNG_CHUNK;
+    case 37: return OT_ERR_PNG_BIT_DEPTH;
+    case 38:
+    case 39:
+    case 103: return OT_ERR_PNG_PALETTE;
+    case 93: return OT_ERR_PNG_DIMENSIONS;
+    case 106: return OT_ERR_PNG_PALETTE;
+
+    case 60:
+    case 61:
+    case 90: return OT_ERR_PNG_WINDOW_SIZE;
+    case 84: return OT_ERR_PNG_ENCODE;
+    case 68:
+    case 71: return OT_ERR_PNG_ENCODE;
+
+    default: return OT_ERR_UNKNOWN;
+  }
 }
 
 const char* ot_error_string(ot_error_t error) {
